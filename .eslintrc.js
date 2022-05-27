52e03f903fbc9b68617c0dcac608117b63815d23
Enes <enessolak99@gmail.com>
2022-05-27 14:08:43 +0300

    [eslintrc] curly rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   rules: {
     semi: [2, 'never'],
     quotes: [2, 'single', 'avoid-escape'],
+    curly: ['error', 'multi', 'consistent'],
     'vue/multi-word-component-names': 'off',
     'vue/html-self-closing': 'off',
     'vue/singleline-html-element-content-newline': 'off'
