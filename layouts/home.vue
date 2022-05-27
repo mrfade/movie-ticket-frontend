87f4d0385da0f35aa35bd5f72f30e67d7c7ca2d9
Enes <enessolak99@gmail.com>
2022-05-28 02:23:48 +0300

    [layouts/home] add page loader

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,5 +1,24 @@
+<script setup>
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+import { useLoaderStore } from '~~/stores/loader'
+const loaderStore = useLoaderStore()
+</script>
+
 <template>
   <div class="relative">
+    <ClientOnly>
+      <loading
+        :active="loaderStore.isLoading"
+        :is-full-page="true"
+        :opacity="0.8"
+        background-color="#000"
+        color="#fff"
+        blur="20px"
+      />
+    </ClientOnly>
+
     <Header transparent />
     <slot />
     <Footer />
