640187858081e55c6e1ac89a3a9dd8ee7105a5a5
Enes <enessolak99@gmail.com>
2022-05-27 12:15:52 +0300

    [plugins/toastification] add toast plugin

---
+++ plugins/toastification.client.ts
@@ -0,0 +1,21 @@
+import Toast, { PluginOptions, POSITION } from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const options: PluginOptions = {
+    position: POSITION.BOTTOM_CENTER,
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: true,
+    hideProgressBar: false,
+    closeButton: 'button',
+    icon: true,
+    rtl: false
+  }
+
+  nuxtApp.vueApp.use(Toast, options)
+})
