1cb7b91a9323e8f92203194294049ac414033e72
Enes <enessolak99@gmail.com>
2022-06-02 18:19:08 +0300

    [components/cast/skeleton] create component

---
+++ components/cast/Skeleton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="animate-pulse flex flex-col items-center">
+    <div class="mb-2 w-24 aspect-[2/3] rounded-lg shadow-lg bg-cod-gray-100 dark:bg-cod-gray-700"></div>
+    <div class="mb-1 w-3/4 h-4 rounded-full bg-cod-gray-100 dark:bg-cod-gray-700"></div>
+    <div class="mb-2 w-2/3 h-4 rounded-full bg-cod-gray-100 dark:bg-cod-gray-700"></div>
+    <div class="w-1/2 h-2 rounded-full bg-cod-gray-100 dark:bg-cod-gray-700"></div>
+  </div>
+</template>
