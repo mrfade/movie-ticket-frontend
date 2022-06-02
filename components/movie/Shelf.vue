76a92d2367df880337a507b25d72a4f9a4031cae
Enes <enessolak99@gmail.com>
2022-06-02 18:18:39 +0300

    [components/movie/shelf] create component

---
+++ components/movie/Shelf.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full max-w-screen-xl mx-auto px-8 py-16">
+    <slot name="title"></slot>
+
+    <div class="grid grid-cols-3 md:grid-cols-6 gap-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
