9ad07c85465042d853e86d677d5a0c95c04feb16
Enes <enessolak99@gmail.com>
2022-06-02 18:19:36 +0300

    [components/movie/search/single] create component

---
+++ components/movie/search/Single.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Movie } from '~~/@types/movie'
+
+defineProps<{
+  movie: Movie
+}>()
+</script>
+
+<template>
+  <nuxt-link :to="`/detail/${movie.slug}`">
+    <div class="flex items-center py-2 space-x-4">
+      <span class="flex-1 text-base font-light text-cod-gray-900">{{ movie.title }}</span>
+      <span class="text-xs px-4 py-1 bg-ywllow text-cod-gray-900 rounded-full">sinema</span>
+    </div>
+  </nuxt-link>
+</template>
