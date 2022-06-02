d53e793f1b981513146b011397b07c0a7f631692
Enes <enessolak99@gmail.com>
2022-06-02 18:19:46 +0300

    [components/movie/search/skeleton] create component

---
+++ components/movie/search/Skeleton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="animate-pulse flex items-center py-2 space-x-4">
+    <div class="flex-1">
+      <div class="h-4 bg-cod-gray-100 rounded-full"></div>
+    </div>
+    <div class="h-4 w-20 bg-cod-gray-100 rounded-full"></div>
+  </div>
+</template>
