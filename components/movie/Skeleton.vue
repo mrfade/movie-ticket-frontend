aaa40b1cfcdcdf0a2cda6bb5fc2a00ea386bf38c
Enes <enessolak99@gmail.com>
2022-06-02 18:18:51 +0300

    [components/movie/skeleton] create component

---
+++ components/movie/Skeleton.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="animate-pulse flex flex-col items-center rounded-lg">
+    <div class="w-full rounded-lg aspect-[2/3] bg-cod-gray-100 dark:bg-cod-gray-700"></div>
+    <div class="mt-2 w-full h-4 rounded-full bg-cod-gray-100 dark:bg-cod-gray-700"></div>
+  </div>
+</template>
