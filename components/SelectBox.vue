79682a75aa236b9fd1b048cfe4e60c132d884659
Enes <enessolak99@gmail.com>
2022-06-11 23:57:17 +0300

    [components/SelectBox] add new component

---
+++ components/SelectBox.vue
@@ -0,0 +1,126 @@
+<script setup lang="ts">
+import { Ref } from 'vue'
+import { Listbox, ListboxButton, ListboxLabel, ListboxOption, ListboxOptions } from '@headlessui/vue'
+import { CheckIcon, SelectorIcon } from '@heroicons/vue/solid'
+
+export interface SelectBoxOption {
+  value: string | number,
+  label: string | number,
+  image?: string
+}
+
+interface Props {
+  modelValue: SelectBoxOption | string | number,
+  options: SelectBoxOption[],
+  label?: string,
+  transparentButton?: boolean,
+  selectMinWidth?: number,
+  imageShape?: 'square' | 'rounded' | 'circle',
+  imageAspectRatio?: '1/1' | '1/2' | '2/1' | '3/4' | '4/3' | '16/9' | '9/16',
+  showIcon?: boolean,
+  showRing?: boolean,
+  showImageInLabel?: boolean,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: '',
+  transparentButton: false,
+  selectMinWidth: 0,
+  imageShape: 'circle',
+  imageAspectRatio: '1/1',
+  showIcon: true,
+  showRing: true,
+  showImageInLabel: true
+})
+
+// eslint-disable-next-line func-call-spacing
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: SelectBoxOption): void
+}>()
+
+const currentValue: SelectBoxOption = typeof props.modelValue in ['number', 'string'] ? props.options.find(option => option.value === props.modelValue) : props.modelValue as SelectBoxOption
+const selected: Ref<SelectBoxOption> = ref<SelectBoxOption>(currentValue)
+
+watch(selected, (value: SelectBoxOption) => {
+  emit('update:modelValue', value)
+})
+</script>
+
+<template>
+  <Listbox v-model="selected" as="div">
+    <ListboxLabel v-if="label" class="block text-sm font-medium text-gray-700">{{ label }}</ListboxLabel>
+    <div class="relative" :class="{ 'mt-1': label }">
+      <ListboxButton
+        class="relative w-full text-left py-2 focus:outline-none"
+        :class="[
+          transparentButton ? 'text-white font-base font-medium cursor-pointer' : 'pl-3 pr-10 cursor-default bg-white border border-gray-300 rounded-md shadow-sm sm:text-sm',
+          showRing ? 'focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500' : ''
+        ]"
+      >
+        <span class="flex items-center">
+          <img
+            :key="`selecbox-img-${selected.value}`"
+            :src="selected.image"
+            :alt="(selected.label as string)"
+            class="flex-shrink-0 h-6"
+            :class="{
+              'rounded-full': imageShape === 'circle',
+              'rounded-lg': imageShape === 'rounded',
+              'rounded-none': imageShape === 'square',
+              'aspect-[1/2]': imageAspectRatio === '1/2',
+              'aspect-[2/1]': imageAspectRatio === '2/1',
+              'aspect-[3/4]': imageAspectRatio === '3/4',
+              'aspect-[4/3]': imageAspectRatio === '4/3',
+              'aspect-[16/9]': imageAspectRatio === '16/9',
+              'aspect-[9/16]': imageAspectRatio === '9/16'
+            }"
+          />
+          <span class="ml-3 block truncate">{{ selected.label }}</span>
+        </span>
+        <span v-if="showIcon" class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+          <SelectorIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+        </span>
+      </ListboxButton>
+
+      <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+        <ListboxOptions
+          class="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+          :style="{
+            minWidth: selectMinWidth ? selectMinWidth + 'px' : '',
+          }"
+        >
+          <ListboxOption v-for="option in options" :key="`option-${option.value}`" v-slot="{ active, selected }" as="template" :value="option">
+            <li :class="[active ? 'text-white bg-indigo-600' : 'text-gray-900', 'cursor-pointer select-none relative py-2 pl-3 pr-9']">
+              <div class="flex items-center">
+                <img
+                  v-if="showImageInLabel"
+                  :src="option.image"
+                  alt=""
+                  class="flex-shrink-0 h-6 rounded-full"
+                  :class="{
+                    'rounded-full': imageShape === 'circle',
+                    'rounded-lg': imageShape === 'rounded',
+                    'rounded-none': imageShape === 'square',
+                    'aspect-[1/2]': imageAspectRatio === '1/2',
+                    'aspect-[2/1]': imageAspectRatio === '2/1',
+                    'aspect-[3/4]': imageAspectRatio === '3/4',
+                    'aspect-[4/3]': imageAspectRatio === '4/3',
+                    'aspect-[16/9]': imageAspectRatio === '16/9',
+                    'aspect-[9/16]': imageAspectRatio === '9/16'
+                  }"
+                />
+                <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate']">
+                  {{ option.label }}
+                </span>
+              </div>
+
+              <span v-if="selected" :class="[active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4']">
+                <CheckIcon class="h-5 w-5" aria-hidden="true" />
+              </span>
+            </li>
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
+    </div>
+  </Listbox>
+</template>
