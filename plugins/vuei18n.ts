af15b288cff2cfcb4672f76a6f4b3575265d69b6
Enes <enessolak99@gmail.com>
2022-06-11 23:58:46 +0300

    [plugins/vuei18n] improve

--- plugins/vuei18n.ts
+++ plugins/vuei18n.ts
@@ -1,4 +1,5 @@
 import { createI18n } from 'vue-i18n'
+import { useStorage } from '@vueuse/core'
 
 import tr from '~~/locales/tr.json'
 import en from '~~/locales/en.json'
@@ -6,6 +7,7 @@ import en from '~~/locales/en.json'
 const vuei18n = createI18n({
   legacy: false,
   globalInjection: true,
+  allowComposition: true,
   locale: 'tr',
   fallbackLocale: 'en',
   messages: {
@@ -16,4 +18,9 @@ const vuei18n = createI18n({
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(vuei18n)
+
+  nuxtApp.hook('app:mounted', () => {
+    const _locale = useStorage('locale', 'tr')
+    vuei18n.global.locale.value = _locale.value
+  })
 })
