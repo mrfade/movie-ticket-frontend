edbceb6611c7854bfad9d9ac60232521fa4c2b19
Enes <enessolak99@gmail.com>
2022-06-11 22:52:41 +0300

    [pages/profile] fix coloring

--- pages/profile.vue
+++ pages/profile.vue
@@ -23,7 +23,7 @@ definePageMeta({
         <div class="col-span-3 lg:col-span-1">
           <div class="flex flex-col gap-4">
             <nuxt-link v-for="link in links" :key="link.path" :to="link.path">
-              <div class="flex items-center p-4 dark:text-cod-gray-200 bg-cod-gray-100 dark:bg-cod-gray-850 hover:bg-cod-gray-200 hover:dark:bg-cod-gray-800 rounded-lg">
+              <div class="flex items-center p-4 text-cod-gray-800 dark:text-cod-gray-200 bg-white dark:bg-cod-gray-850 hover:bg-cod-gray-100 hover:dark:bg-cod-gray-800 rounded-lg">
                 {{ link.name }}
               </div>
             </nuxt-link>
