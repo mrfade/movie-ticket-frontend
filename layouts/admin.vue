2a2fb068b9cb561d604d2b73a68f20d9becbbd0f
Enes <enessolak99@gmail.com>
2022-06-12 18:50:43 +0300

    [layout/admin] add admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+import { useLoaderStore } from '~~/stores/loader'
+const loaderStore = useLoaderStore()
+
+useDark()
+</script>
+
+<template>
+  <div>
+    <ClientOnly>
+      <loading
+        :active="loaderStore.isLoading"
+        :is-full-page="true"
+        :opacity="0.8"
+        background-color="#000"
+        color="#fff"
+      />
+    </ClientOnly>
+
+    <div class="flex flex-col lg:flex-row">
+      <admin-sidebar class="w-full lg:fixed lg:w-64 lg:h-screen" />
+      <div class="lg:ml-64 lg:flex-1 lg:overflow-x-auto">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  @apply bg-gray-100 dark:bg-gray-900;
+}
+</style>
