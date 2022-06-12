5e649ae34ea0f2190b68656a6a1f2d8002f11598
Enes <enessolak99@gmail.com>
2022-06-12 11:52:17 +0300

    [components/SelectCity] switch to selectbox

--- components/SelectCity.vue
+++ components/SelectCity.vue
@@ -1,37 +1,38 @@
 <script setup lang="ts">
+import { Ref } from 'vue'
+import { SelectBoxOption } from './SelectBox.vue'
 import { City } from '~~/@types/city'
 import { useCityStore } from '~~/stores/city'
 
 const cityStore = useCityStore()
 
-const selectedCityId = ref<string>(cityStore.selectedCity?.id.toString())
+const selectedCity: Ref<SelectBoxOption> = ref<SelectBoxOption>({
+  value: cityStore.selectedCity?.id.toString(),
+  label: cityStore.selectedCity?.name
+})
 
 const cities: City[] = cityStore.getCities
-// const isSelected = (city: City) => cityStore.selectedCity?.id === city.id
+const options: SelectBoxOption[] = cities.map(city => ({
+  value: city.id.toString(),
+  label: city.name
+} as SelectBoxOption))
 
 const cityIdCookie = useCookie('cityId')
 
-watch(selectedCityId, (id) => {
-  cityStore.setSelectedCityId(parseInt(id))
-  cityIdCookie.value = id
+watch(selectedCity, (option: SelectBoxOption) => {
+  cityStore.setSelectedCityId(parseInt(option.value))
+  cityIdCookie.value = option.value
 })
 </script>
 
 <template>
   <div class="w-auto">
-    <label for="city-selector" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Şehir Seçin</label>
-    <select
+    <select-box
       id="city-selector"
-      v-model="selectedCityId"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-auto p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    >
-      <option
-        v-for="city in cities"
-        :key="city.id"
-        :value="city.id"
-      >
-        {{ city.name }}
-      </option>
-    </select>
+      v-model="selectedCity"
+      label="Şehir Seçin"
+      :select-min-width="150"
+      :options="options"
+    />
   </div>
 </template>
