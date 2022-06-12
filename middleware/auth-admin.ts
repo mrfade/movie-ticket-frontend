1b85ad926c4e7859104f69906d062e5d590fc947
Enes <enessolak99@gmail.com>
2022-06-12 18:51:23 +0300

    [middleware/auth-admin] add admin middleware

---
+++ middleware/auth-admin.ts
@@ -0,0 +1,17 @@
+import { createError } from 'h3'
+import { useUserStore } from '~~/stores/user'
+
+export default defineNuxtRouteMiddleware((_to, from) => {
+  const userStore = useUserStore()
+
+  if (!userStore.isAuthenticated)
+    return `/login?next=${encodeURIComponent(from.fullPath)}`
+
+  if (!userStore.isAdmin)
+    return throwError(
+      createError({
+        statusCode: 401,
+        statusMessage: 'You must be an admin to access this page.'
+      })
+    )
+})
