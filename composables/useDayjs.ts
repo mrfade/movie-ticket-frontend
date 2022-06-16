dae11b85f312eadbef374f1111a09f921fa7db4f
Enes <enessolak99@gmail.com>
2022-06-17 02:20:42 +0300

    [composables/useDayjs] import relativeTime

--- composables/useDayjs.ts
+++ composables/useDayjs.ts
@@ -1,11 +1,13 @@
 import { useStorage } from '@vueuse/core'
 import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/tr'
 
 const locale = useStorage('locale', 'tr')
 
 const useDayjs = () => {
   dayjs.locale(locale.value)
+  dayjs.extend(relativeTime)
   return dayjs
 }
 
